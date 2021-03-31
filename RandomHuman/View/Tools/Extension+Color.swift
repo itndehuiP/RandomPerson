572e5ba8fe1f56b6dc701cd19e43c0ed501c1f77
itndehuip <itandehui_pacheco@hotmail.com>
2021-03-31 01:12:39 -0700

    Color palette added

---
+++ RandomHuman/View/Tools/Extension+Color.swift
@@ -0,0 +1,17 @@
+//
+//  Extension+Color.swift
+//  RandomHuman
+//
+//  Created by Guerson Perez on 31/03/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let alert = UIColor(named: "Alert")
+    static let constrast = UIColor(named: "Contrast")
+    static let highlight = UIColor(named: "Highlight")
+    static let secondHighlight = UIColor(named: "SecondHighlight")
+    static let theme = UIColor(named: "Theme")
+}
